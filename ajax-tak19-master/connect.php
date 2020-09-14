c41aef95c29ee9b590331429b1411a84965e764d
fs-s <55434294+fs-s@users.noreply.github.com>
2020-09-14 08:57:05 +0300

    Add files via upload

---
+++ ajax-tak19-master/connect.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = 'd83305.mysql.zonevs.eu';
+$db   = 'd83305_books';
+$user  = 'd83305sa325721';
+$pass = 'Ithakapyta1';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
