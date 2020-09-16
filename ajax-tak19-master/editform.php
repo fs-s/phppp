c835e8420eb8adc550941b82fc5b974dd11c97bb
fs-s <55434294+fs-s@users.noreply.github.com>
2020-09-16 11:41:55 +0300

    Create editform.php

---
+++ ajax-tak19-master/editform.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'connect.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
